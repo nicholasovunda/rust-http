e27c668446a3d1cb9a80552d09175e1699adca82
nicholas <nicholasovunda@gmail.com>
2022-07-15 14:57:34 +0100

    completed course

--- src/server.rs
+++ src/server.rs
@@ -1,8 +1,18 @@
-use crate::http::Request;
+use crate::http::ParseError;
+use crate::http::{Request, Response, StatusCode};
 use std::convert::TryFrom;
 use std::convert::TryInto;
-use std::io::Read;
+use std::io::{Read, Write};
 use std::net::TcpListener;
+
+pub trait Handler {
+    fn handle_request(&mut self, request: &Request) -> Response;
+
+    fn handle_bad_request(&mut self, e: &ParseError) -> Response {
+        println!("Failed to parse request: {}", e);
+        Response::new(StatusCode::BadRequest, None)
+    }
+}
 pub struct Server {
     addr: String,
 }
@@ -12,7 +22,7 @@ impl Server {
     pub fn new(addr: String) -> Self {
         Self { addr: addr }
     }
-    pub fn run(self) {
+    pub fn run(self, mut handler: impl Handler) {
         println!("Listening on {}", self.addr);
         let listener = TcpListener::bind(&self.addr).unwrap();
 
@@ -23,11 +33,16 @@ impl Server {
                     match stream.read(&mut buffer) {
                         Ok(_) => {
                             println!("Received a request {}", String::from_utf8_lossy(&buffer));
-                            match Request::try_from(&buffer[..]) {
-                                Ok(request) => {}
-                                Err(e) => println!("Failed to pass Request: {}", e),
+                            let response = match Request::try_from(&buffer[..]) {
+                                Ok(request) => handler.handle_request(&request),
+
+                                Err(e) => handler.handle_bad_request(&e),
+                            };
+                            if let Err(e) = response.send(&mut stream) {
+                                println!("Failed to send response: {}", e)
                             }
-                            let res: &Result<Request, _> = &buffer[..].try_into();
+
+                            // let res: &Result<Request, _> = &buffer[..].try_into();
                         }
                         Err(e) => println!("Failed to establish connection {}", e),
                     }
